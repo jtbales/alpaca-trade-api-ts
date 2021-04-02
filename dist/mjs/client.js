0ce030c795360a0cb8bb675de24c7ef0d9392972
John Bales <jtbales@users.noreply.github.com>
2021-04-02 16:22:56 -0500

    back to the all build

---
+++ dist/mjs/client.js
@@ -0,0 +1,285 @@
+import Bottleneck from 'bottleneck';
+import qs from 'qs';
+import isofetch from 'isomorphic-unfetch';
+import urls from './urls.js';
+import parse from './parse.js';
+const unifetch = typeof fetch !== 'undefined' ? fetch : isofetch;
+export class AlpacaClient {
+    constructor(params) {
+        this.params = params;
+        this.limiter = new Bottleneck({
+            reservoir: 200,
+            reservoirRefreshAmount: 200,
+            reservoirRefreshInterval: 60 * 1000,
+            // also use maxConcurrent and/or minTime for safety
+            maxConcurrent: 1,
+            minTime: 200,
+        });
+        if (
+        // if not specified
+        !('paper' in params.credentials) &&
+            // and live key isn't already provided
+            !('key' in params.credentials && params.credentials.key.startsWith('A'))) {
+            params.credentials['paper'] = true;
+        }
+        if ('access_token' in params.credentials &&
+            ('key' in params.credentials || 'secret' in params.credentials)) {
+            throw new Error("can't create client with both default and oauth credentials");
+        }
+    }
+    async isAuthenticated() {
+        try {
+            await this.getAccount();
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+    async getAccount() {
+        return parse.account(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/account`,
+        }));
+    }
+    async getOrder(params) {
+        return parse.order(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/orders/${params.order_id || params.client_order_id}`,
+            data: { nested: params.nested },
+        }));
+    }
+    async getOrders(params = {}) {
+        return parse.orders(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/orders`,
+            data: {
+                ...params,
+                symbols: params.symbols ? params.symbols.join(',') : undefined,
+            },
+        }));
+    }
+    async placeOrder(params) {
+        return parse.order(await this.request({
+            method: 'POST',
+            url: `${urls.rest.account}/orders`,
+            data: params,
+        }));
+    }
+    async replaceOrder(params) {
+        return parse.order(await this.request({
+            method: 'PATCH',
+            url: `${urls.rest.account}/orders/${params.order_id}`,
+            data: params,
+        }));
+    }
+    cancelOrder(params) {
+        return this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/orders/${params.order_id}`,
+            isJSON: false,
+        });
+    }
+    async cancelOrders() {
+        return parse.canceled_orders(await this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/orders`,
+        }));
+    }
+    async getPosition(params) {
+        return parse.position(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/positions/${params.symbol}`,
+        }));
+    }
+    async getPositions() {
+        return parse.positions(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/positions`,
+        }));
+    }
+    async closePosition(params) {
+        return parse.order(await this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/positions/${params.symbol}`,
+        }));
+    }
+    async closePositions() {
+        return parse.orders(await this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/positions`,
+        }));
+    }
+    getAsset(params) {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/assets/${params.asset_id_or_symbol}`,
+        });
+    }
+    getAssets(params) {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/assets`,
+            data: params,
+        });
+    }
+    getWatchlist(params) {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/watchlists/${params.uuid}`,
+        });
+    }
+    getWatchlists() {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/watchlists`,
+        });
+    }
+    createWatchlist(params) {
+        return this.request({
+            method: 'POST',
+            url: `${urls.rest.account}/watchlists`,
+            data: params,
+        });
+    }
+    updateWatchlist(params) {
+        return this.request({
+            method: 'PUT',
+            url: `${urls.rest.account}/watchlists/${params.uuid}`,
+            data: params,
+        });
+    }
+    addToWatchlist(params) {
+        return this.request({
+            method: 'POST',
+            url: `${urls.rest.account}/watchlists/${params.uuid}`,
+            data: params,
+        });
+    }
+    removeFromWatchlist(params) {
+        return this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/watchlists/${params.uuid}/${params.symbol}`,
+        });
+    }
+    deleteWatchlist(params) {
+        return this.request({
+            method: 'DELETE',
+            url: `${urls.rest.account}/watchlists/${params.uuid}`,
+        });
+    }
+    getCalendar(params) {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/calendar`,
+            data: params,
+        });
+    }
+    async getClock() {
+        return parse.clock(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/clock`,
+        }));
+    }
+    getAccountConfigurations() {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/account/configurations`,
+        });
+    }
+    updateAccountConfigurations(params) {
+        return this.request({
+            method: 'PATCH',
+            url: `${urls.rest.account}/account/configurations`,
+            data: params,
+        });
+    }
+    async getAccountActivities(params) {
+        if (params.activity_types && Array.isArray(params.activity_types)) {
+            params.activity_types = params.activity_types.join(',');
+        }
+        return parse.activities(await this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/account/activities${params.activity_type ? '/'.concat(params.activity_type) : ''}`,
+            data: { ...params, activity_type: undefined },
+        }));
+    }
+    getPortfolioHistory(params) {
+        return this.request({
+            method: 'GET',
+            url: `${urls.rest.account}/account/portfolio/history`,
+            data: params,
+        });
+    }
+    async getTrades(params) {
+        return parse.pageOfTrades(await this.request({
+            method: 'GET',
+            url: `${urls.rest.market_data}/stocks/${params.symbol}/trades`,
+            data: { ...params, symbol: undefined },
+        }));
+    }
+    async getQuotes(params) {
+        return parse.pageOfQuotes(await this.request({
+            method: 'GET',
+            url: `${urls.rest.market_data}/stocks/${params.symbol}/quotes`,
+            data: { ...params, symbol: undefined },
+        }));
+    }
+    async getBars(params) {
+        return parse.pageOfBars(await this.request({
+            method: 'GET',
+            url: `${urls.rest.market_data}/stocks/${params.symbol}/bars`,
+            data: { ...params, symbol: undefined },
+        }));
+    }
+    async request(params) {
+        let headers = {};
+        if ('access_token' in this.params.credentials) {
+            headers['Authorization'] = `Bearer ${this.params.credentials.access_token}`;
+        }
+        else {
+            headers['APCA-API-KEY-ID'] = this.params.credentials.key;
+            headers['APCA-API-SECRET-KEY'] = this.params.credentials.secret;
+        }
+        if (this.params.credentials.paper) {
+            params.url = params.url.replace('api.', 'paper-api.');
+        }
+        let query = '';
+        if (params.data) {
+            // translate dates to ISO strings
+            for (let [key, value] of Object.entries(params.data)) {
+                if (value instanceof Date) {
+                    params.data[key] = value.toISOString();
+                }
+            }
+            // build query
+            if (params.method != 'POST' && params.method != 'PATCH') {
+                query = '?'.concat(qs.stringify(params.data));
+                params.data = undefined;
+            }
+        }
+        const makeCall = () => unifetch(params.url.concat(query), {
+            method: params.method,
+            headers,
+            body: JSON.stringify(params.data),
+        }), func = this.params.rate_limit
+            ? () => this.limiter.schedule(makeCall)
+            : makeCall;
+        let resp, result = {};
+        try {
+            resp = await func();
+            if (!(params.isJSON == undefined ? true : params.isJSON)) {
+                return resp.ok;
+            }
+            result = await resp.json();
+        }
+        catch (e) {
+            console.error(e);
+            throw result;
+        }
+        if ('code' in result || 'message' in result) {
+            throw result;
+        }
+        return result;
+    }
+}
---
+++ dist/mjs/urls.js
@@ -0,0 +1,10 @@
+export default {
+    rest: {
+        account: 'https://api.alpaca.markets/v2',
+        market_data: 'https://data.alpaca.markets/v2',
+    },
+    websocket: {
+        account: 'wss://api.alpaca.markets/stream',
+        market_data: (source = 'iex') => `wss://stream.data.alpaca.markets/v2/${source}`,
+    },
+};
