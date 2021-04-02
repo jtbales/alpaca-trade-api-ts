b04a5498d77bbc168fcb6b2da2bd9c18cfeb9c29
John Bales <jtbales@users.noreply.github.com>
2021-04-02 14:48:01 -0500

    lazy ass flat build

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/alpaca.min.js')
-} else {
-  module.exports = require('./dist/alpaca.js')
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AlpacaStream = exports.AlpacaClient = void 0;
+var client_js_1 = require("./client.js");
+Object.defineProperty(exports, "AlpacaClient", { enumerable: true, get: function () { return client_js_1.AlpacaClient; } });
+var stream_js_1 = require("./stream.js");
+Object.defineProperty(exports, "AlpacaStream", { enumerable: true, get: function () { return stream_js_1.AlpacaStream; } });
+const client_js_2 = require("./client.js");
+const stream_js_2 = require("./stream.js");
+exports.default = {
+    AlpacaClient: client_js_2.AlpacaClient,
+    AlpacaStream: stream_js_2.AlpacaStream,
+};
